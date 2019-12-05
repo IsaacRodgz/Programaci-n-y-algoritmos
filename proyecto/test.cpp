107fb635d00cfd6c58c1b3b6ce22e3797e47b35f
IsaacRodgz <chack.rb@gmail.com>
2019-12-05 11:57:04 -0600

    [ADD] Random map

--- proyecto/test.cpp
+++ proyecto/test.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-
+    /*
     vector<vector<double> > world = {
 
         { 1, 0, 0, 1, 0, 2 },
@@ -26,7 +26,7 @@ int main(int argc, char const *argv[]) {
         { 1, 0, 2, 1, 2, 0 },
         { 1, 1, 2, 1, 1, 1 }
     };
-
+    */
 
     /*
     vector<vector<double> > world = {
@@ -42,6 +42,63 @@ int main(int argc, char const *argv[]) {
         { 1, 1, 1, 1, 1, 1, 1, 1, 1 }
     };
     */
+
+    /*
+    vector<vector<double> > world = {
+
+        { 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 2, 2.5, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 },
+        { 2.5, 1, 1, 1, 2.5, 1, 0, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 2, 1, 0, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1 },
+        { 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 2.5 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 2.5, 1, 1, 1, 1, 1, 1 },
+        { 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2.5, 1 },
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1 },
+        { 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1 }
+    };
+    */
+
+    random_device rd;
+    mt19937 eng(rd());
+    uniform_real_distribution<> distr(0, 1);
+
+    int w = 20;
+    int h = 20;
+
+    vector<vector<double> > world;
+    world.resize(w, vector<double>(h, 0));
+
+    for (int i = 0; i < w; i++) {
+
+        for (int j = 0; j < h; j++) {
+
+            double val = distr(eng);
+
+            if ( val < 0.1 ) {
+
+                world[i][j] = 0.0;
+            }
+
+            else if ( val < 0.5 ) {
+
+                world[i][j] = 1.0;
+            }
+
+            else if ( val < 0.75 ) {
+
+                world[i][j] = 2.0;
+            }
+
+            else {
+
+                world[i][j] = 2.5;
+            }
+        }
+    }
+
     Program p = Program(600, 600, world);
     p.run();
 
